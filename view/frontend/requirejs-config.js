d2e7134daa7fe74beddb2811b1c760b5eed11e7a
WebRotate 360 <support@webrotate360.com>
2017-02-20 17:34:05 -0800

    Revert "Initial master commit of version 2.0.0."
    This reverts commit dd0d8bfbb362edd63a3a4a4ba32029818ca34a78.

--- view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            'imagerotator': 'WebRotate360_ProductViewerAdHoc/imagerotator/html/js/imagerotator',
-            'wr360hook': 'WebRotate360_ProductViewerAdHoc/wr360hook',
-            'prettyPhoto': 'WebRotate360_ProductViewerAdHoc/prettyPhoto/js/jquery.prettyPhoto'
-        }
-    }
-};
